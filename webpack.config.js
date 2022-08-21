318a0487e23a71e6d1fc740776ddec0abf52521b
Денис Остапенко <ostapenko.devs@gmail.com>
2022-08-21 15:58:13 +0300

    настройка Autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -35,9 +35,16 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'postcss-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, 'postcss.config.js'),
+                            },
+                        },
+                    },
                     'sass-loader',
-                ],
+                ]
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
